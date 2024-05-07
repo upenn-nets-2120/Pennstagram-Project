93d462532a4807c4625587d2153077a3c784345e
JoshWeissman <jlw1491625@hotmail.com>
2024-05-07 16:53:26 -0400

    added theme provider

---
+++ frontend/src/constants/themes.ts
@@ -0,0 +1,15 @@
+import { Theme } from "../entities/Theme";
+
+export const lightTheme: Theme = {
+    mode: 'light',
+    primaryColor: '#007bff',
+    secondaryColor: '#6c757d',
+    tertiaryColor: '#6c757d'
+};
+
+export const darkTheme: Theme = {
+    mode: 'dark',
+    primaryColor: '#17a2b8',
+    secondaryColor: '#343a40',
+    tertiaryColor: '#343a40',
+};
