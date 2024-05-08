71e9767c89d08894d4a42052d606035445709d89
JoshWeissman <jlw1491625@hotmail.com>
2024-05-08 18:26:23 -0400

    user provider

--- frontend/src/providers/UserProvider.tsx
+++ frontend/src/providers/UserProvider.tsx
@@ -1,1 +1,36 @@
-export const hi = 'ignore';
+import React, { useState } from 'react';
+import { User } from '../entities/User';
+
+export const UserContext = React.createContext<{
+    user: User | undefined;
+    login: (user: User) => void; // Update the login function signature
+    logout: () => void;
+}>({
+    user: undefined,
+    login: () => {},
+    logout: () => {},
+});
+
+interface UserProviderProps {
+    children: React.ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+    const [user, setUser] = useState<User | undefined>(undefined);
+
+    const login = (user: User) => {
+        setUser(user);
+    }
+
+    const logout = () => {
+        setUser(undefined);
+    }
+
+    return (
+        <UserContext.Provider value={{ user, login, logout }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserProvider;
