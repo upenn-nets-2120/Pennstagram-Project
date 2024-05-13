6bd413b4762b1ef88efa817f826992727e31a4ad
Alain Welliver <alainwelliver@gmail.com>
2024-05-13 11:13:14 +0000

    RAG

--- backend/src/db-operations/search/get-search-result.js
+++ backend/src/db-operations/search/get-search-result.js
@@ -1,6 +1,8 @@
 import { ChatOpenAI, OpenAIEmbeddings } from "@langchain/openai";
 import { formatDocumentsAsString } from "langchain/util/document";
+import { StringOutputParser } from "@langchain/core/output_parsers";
 import { RunnableSequence, RunnablePassthrough } from "@langchain/core/runnables";
+import { PromptTemplate } from "@langchain/core/prompts";
 import { Chroma } from "@langchain/community/vectorstores/chroma";
 import config from './config.json' assert { type: "json" };
 
@@ -9,9 +11,9 @@ let vectorStore = null;
 const getVectorStore = async function() {
     if (vectorStore == null) {
         vectorStore = await Chroma.fromExistingCollection(new OpenAIEmbeddings({
-            apiKey: config.open_ai_key // Pass the API key here
+            apiKey: config.open_ai_key
         }), {
-            collectionName: "all_appdata_embeddings", // Use the combined collection
+            collectionName: "all_appdata_embeddings",
             url: "http://localhost:8000",
         });
     }
@@ -22,24 +24,33 @@ const getSearchResult = async (username, context, searchQuery) => {
     const vs = await getVectorStore();
     const retriever = vs.asRetriever();
 
-    const prompt = `Use the following pieces of context to answer the Search Query at the end:
+    // Ensure prompt is formatted as a runnable or function
+    const prompt = PromptTemplate.fromTemplate(`Use the following pieces of context to answer the Search Query at the end. Feel free to make up information about users or actors you know about 
+    Always say "thanks for asking!" at the end of the answer.
+    
     ${context}
+    
     Search Query: ${searchQuery}
-    Helpful Answer:`;
-
+        
+    Helpful Answer:`);
     const llm = new ChatOpenAI({
-        apiKey: config.open_ai_key, // Ensure API key is correctly referenced here too
-        model: "gpt-3.5-turbo",
+        apiKey: config.open_ai_key,
+        model: "gpt-3.5-turbo-16k-0613",
         temperature: 0
     });
 
     const ragChain = RunnableSequence.from([
-        retriever.pipe(formatDocumentsAsString),
-        prompt,
-        llm,
+        {
+            context: retriever.pipe(formatDocumentsAsString),
+            question: new RunnablePassthrough(),
+          },
+      prompt,
+      llm,
+      new StringOutputParser(),
     ]);
 
-    const result = await ragChain.run(searchQuery);
+    let result = await ragChain.invoke(searchQuery);
+
     return result;
 }
 
