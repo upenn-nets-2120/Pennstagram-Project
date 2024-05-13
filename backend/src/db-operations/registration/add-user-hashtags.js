5dfe5e0a226dfe6356887c5369a423f832780b84
SemF1 <136936776+SemF1@users.noreply.github.com>
2024-05-13 00:35:17 -0400

    Login & Registration 99% implemented

--- backend/src/db-operations/registration/add-user-hashtags.js
+++ backend/src/db-operations/registration/add-user-hashtags.js
@@ -1,12 +1,23 @@
 import db from '../../db-setup/db_access.js';
 
-const addUserHashtags = async (username, hashtags) => {
-    const sql = `
-        INSERT INTO users2hashtags (username, hashtags)
-        VALUES ${hashtags.map(hashtags => `('${username}', '${hashtags}')`).join(', ')}
-    ;`;
-
-    return await db.insert_items(sql);
+const addUserHashtags = async (userID, hashtags) => {
+    let cleaned;
+    let data;
+    for (const hashtag of hashtags) {
+        cleaned = hashtag.label.replace(/^#/, '')
+        try {
+            // Step 1: Insert new hashtag
+            data = await db.send_sql(`INSERT INTO hashtags (phrase) VALUES ('${cleaned}')`);
+            // Step 2: Retrieve the last insert ID
+            // const hashtagIDResult = await db.send_sql('SELECT LAST_INSERT_ID() as hashtagID');
+            console.log("sdjfsdljkf:", data.insertId);
+            const hashtagID = data.insertId;
+            // Step 3: Insert into users2hashtags table
+            await db.send_sql(`INSERT INTO users2hashtags (userID, hashtagID) VALUES (${userID}, ${hashtagID})`);
+        } catch (error) {
+            console.error('Failed to insert hashtag:', hashtag.label, 'Error:', error);
+        }
+    }
 }
 
 export default addUserHashtags;
